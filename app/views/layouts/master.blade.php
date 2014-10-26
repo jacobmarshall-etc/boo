743012ef19e15ebcdbdc82b0a82357de63e83aa5
Jacob Marshall <jacob+github@manage.net.nz>
2014-10-26 19:20:31 +1300

    Test CloudFlare proxy fix

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,9 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="{{ secure_asset('assets/css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
 </head>
 <body>
     @yield('content')
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
 </body>
 </html>
