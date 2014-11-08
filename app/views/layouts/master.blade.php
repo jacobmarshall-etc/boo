6255f9c5782eb51c123a05bba130520daf04198b
Jacob Marshall <jacob+github@manage.net.nz>
2014-11-08 21:11:25 +1300

    Remove html comments

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -7,17 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Stylesheet -->
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
 </head>
 <body>
     @yield('content')
 
-    <!-- Javascript -->
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="{{ asset('assets/js/main.js') }}"></script>
 
-    <!-- Typekit -->
     <script src="//use.typekit.net/zjn6avp.js"></script>
     <script>try { Typekit.load(); } catch(e) {}</script>
 </body>
